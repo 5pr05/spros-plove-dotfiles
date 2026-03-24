60b4d62300c579023f6854eaef1ed7559958e35b
Isfandiyar <147981172+5pr05@users.noreply.github.com>
2026-03-24 21:21:41 +0100

    add installation script

---
+++ install_packages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo pacman -Syu
+
+sudo pacman -S --needed \
+    swayfx \
+    waybar \
+    foot \
+    swaybg \
+    wdisplays \
+    wofi
+
+sudo pacman -S --needed \
+    zsh \
+    fzf \
+    zoxide
+
+sudo pacman -S --needed \
+    pipewire-pulse \
+    pavucontrol \
+    brightnessctl \
+    bluez \
+    bluez-utils
+
+yay -S --needed \
+    ttf-jetbrains-mono-nerd \
+    ttf-font-awesome \
+    ttf-nerd-fonts-symbols \
+    ttf-apple-emoji \
+    everforest-gtk-theme-git \
+    gruvbox-plus-icon-theme
